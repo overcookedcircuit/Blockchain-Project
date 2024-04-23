3fef43c89d01598c9c9d27120faeba71a74e8898
oveecookedcircuit <manaspatel31122004@gmail.com>
2024-04-23 16:22:14 -0400

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "2024/04/23", "Genesis block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let coin = new Blockchain();
+coin.addBlock(new Block(1, "2024/04/15", {amount: 4}));
+coin.addBlock(new Block(2, "2024/04/16", {amount: 5}));
+
+console.log(JSON.stringify(coin, null, 4));
