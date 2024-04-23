5eb275aabb5e5babe5bf2562f3788da8321fa626
oveecookedcircuit <manaspatel31122004@gmail.com>
2024-04-23 16:49:46 -0400

    checking for validity of blockchain

--- main.js
+++ main.js
@@ -32,6 +32,24 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for(let i=1; i<this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            //Check if block points to current previous block
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let coin = new Blockchain();
@@ -39,3 +57,14 @@ coin.addBlock(new Block(1, "2024/04/15", {amount: 4}));
 coin.addBlock(new Block(2, "2024/04/16", {amount: 5}));
 
 console.log(JSON.stringify(coin, null, 4));
+console.log('Is blockchain valid? ' + coin.isChainValid());
+
+
+/*
+Tempering with the blockchain
+method returns false
+
+coin.chain[1].data = {amount: 1000};
+coin.chain[1].hash = coin.chain[1].calculateHash();
+console.log('Is blockchain valid? ' + coin.isChainValid());
+*/
