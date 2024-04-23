145a5ed451808a771ba31d0977edabb56b391f92
oveecookedcircuit <manaspatel31122004@gmail.com>
2024-04-23 17:09:32 -0400

    adding mining for proof of work

--- main.js
+++ main.js
@@ -7,16 +7,29 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    //For proof of work
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        //By increasing the difficulty, it takes a much longer process time for the computer to mine the block
+        this.difficulty = 2;
     }
 
     createGenesisBlock(){
@@ -29,7 +42,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -53,11 +66,14 @@ class Blockchain{
 }
 
 let coin = new Blockchain();
+
+console.log('Mining block 1...');
 coin.addBlock(new Block(1, "2024/04/15", {amount: 4}));
+
+console.log('Mining block 2...');
 coin.addBlock(new Block(2, "2024/04/16", {amount: 5}));
 
-console.log(JSON.stringify(coin, null, 4));
-console.log('Is blockchain valid? ' + coin.isChainValid());
+
 
 
 /*
